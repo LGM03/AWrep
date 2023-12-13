257795e6436b4a2173f7cbfd51ba710d4afbd8de
LGM03 <99087911+LGM03@users.noreply.github.com>
2023-12-13 16:49:33 +0100

    ajax en los mensajes uu

--- public/js/mensajes.js
+++ public/js/mensajes.js
@@ -2,18 +2,18 @@ $(document).ready(function () {
 
   $("#botonMensajesRecibidos").on("click", function () {
     $("#mensajesRecibidos").fadeIn(1000)
-    $("#mensajesEnviados").fadeOut(0)
+    $("#mensajesEnviados").hide()
 
     $.ajax({
       method: "GET",
-      url: "/mensajes",
+      url: "/mensajes/leerEnviados",
       data: {  },
       success: function (datos, state, jqXHR) { //Si todo ha ido bien pongo un mensaje de acierto
         if (datos !== "0") {
           $("#CorreoReceptor").text(datos.usuario)
           datos.mensajes.forEach(element => {
             if(element.correoReceptor == datos.usuario.correo){
-              agregarCajaRecibidosMensajes(element)
+              agregarCajaEnviadosMensajes(element,$("#cajamensajesRecibidos"))
             }
           });
         } else {
@@ -28,18 +28,19 @@ $(document).ready(function () {
 
   $("#botonMensajesEnviados").on("click", function () {
     $("#mensajesEnviados").fadeIn(1000)
-    $("#mensajesRecibidos").fadeOut(0)
+    $("#mensajesRecibidos").hide()
 
     $.ajax({
       method: "GET",
-      url: "/mensajes",
+      url: "/mensajes/leerEnviados",
       data: {  },
       success: function (datos, state, jqXHR) { //Si todo ha ido bien pongo un mensaje de acierto
         if (datos !== "0") {
           $("#CorreoEmisor").text(datos.usuario)
           datos.mensajes.forEach(element => {
             if(element.correoEmisor == datos.usuario.correo){
-              agregarCajaEnviadosMensajes(element)
+              console.log(element.correoEmisor)
+              agregarCajaEnviadosMensajes(element,$("#cajamensajesEnviados"))
             }
           });
         } else {
@@ -68,18 +69,45 @@ $(document).ready(function () {
         data: datosMensaje,
         success: function (datos, state, jqXHR) { //Si todo ha ido bien pongo un mensaje de acierto
           if (datos !== "0") {
+            console.log("A")
+            if(!$("#cajamensajesEnviados").hasClass('d-none')){
+              agregarCajaEnviadosMensajes(datosMensaje,$("#cajamensajesEnviados"))
+            }
             alert("Mensaje enviado con éxito ")
           } else {
             alert("No se pudo enviar el mensaje")
           }
         },
         error: function (jqXHR, statusText, errorThrown) { //Si ha ocurrido un error pongo un mensaje de error
-          console.error('Error al enviar el formulario al servidor:', errorThrown);
-          console.error('Error al enviar el formulario al servidor:', statusText);
-          console.error('Error al enviar el formulario al servidor:', jqXHR);
-          alert(errorThrown)
+          alert("Ha ocurrido un error enviando el mensaje")
         }
       });
     
   })
 })
+
+function agregarCajaEnviadosMensajes(mensaje, padre){
+
+  var fecha = mensaje.fecha.slice(0,10)
+  var hora = mensaje.fecha.slice(11,-8)
+  var cajaMensaje = `<div class="row align-items-center text-center m-2 my-2">
+  <div class="col-12 cajamensaje d-flex flex-column">
+      <div class="d-flex justify-content-between align-items-center mb-1">
+          <h5 class="mb-0">
+               ${mensaje.correoEmisor}
+          </h5>
+          <p class="mb-0"><em>
+          ${fecha}   ${hora}
+              </em></p>
+      </div>
+      <p class="mb-0" style="text-align: left;">
+      ${mensaje.cuerpoMensaje}
+      </p>
+  </div>
+</div>`
+
+padre.append(cajaMensaje)
+padre.removeClass("d-none")
+
+
+}
--- routes/mensajes.js
+++ routes/mensajes.js
@@ -11,70 +11,81 @@ const pool = mysql.createPool({
 })
 
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res, next) {  //Cargo la configuracion para la ventana de mensajes
 
     const DAOAp = require("../mysql/daoMensajes")
     const midao = new DAOAp(pool)
     const DAOAp2 = require("../mysql/daoConfig")
     const daoC = new DAOAp2(pool)
 
+    if (global.titulo == null) {
+        daoC.leerConfig((err, configDatos) => {
+            if (err) { // Si hay un error en el acceso a la bd, ponemos la info por defecto
+                global.titulo = "CUM-RIU";
+                global.logo = "/images/logo.png";
+                global.gama = "/css/estiloOscuro.css";
+                global.direccion = "Av. de Madrid"
+            } else { // Si la configuración se obtiene correctamente, la almacenamos en global
+                global.titulo = configDatos.nombre;
+                const base64String = configDatos.logo.toString('base64');
+                const imageUrl = `data:image/png;base64,${base64String}`;
+                global.logo = imageUrl;
+                global.gama = configDatos.gama;
+                global.direccion = configDatos.direccion;
+            } // Renderizamos la página principal con la información de todos los destinos
+            res.render('mensajes', {
+                logo: global.logo, titulo: global.titulo, gama: global.gama,
+                direccion: global.direccion, sesion: req.session.usuario
+            });
+        });
+    } else {
+        res.render('mensajes', {
+            logo: global.logo, titulo: global.titulo, gama: global.gama,
+            direccion: global.direccion, sesion: req.session.usuario
+        });//Cargo la ventana destino con los usuarios no validados
+    }
+
+});
+
+router.get('/leerEnviados', function (req, res, next) {
+
+    const DAOAp = require("../mysql/daoMensajes")
+    const midao = new DAOAp(pool)
+
+
     midao.leerTodos(req.session.usuario.correo, (err, datos) => {  //Leo en la BD los destinos con el id de la url
-        if (err) {
-            res.send("0") //si ocurre un error cargo la ventana de error 
-        }
         if (err) {
             console.log(err)
             res.send("0") //si ocurre un error cargo la ventana de error 
         }
         else {
-            if (global.titulo == null) {
-                daoC.leerConfig((err, configDatos) => {
-                    if (err) { // Si hay un error en el acceso a la bd, ponemos la info por defecto
-                        global.titulo = "CUM-RIU";
-                        global.logo = "/images/logo.png";
-                        global.gama = "/css/estiloOscuro.css";
-                        global.direccion = "Av. de Madrid"
-                    } else { // Si la configuración se obtiene correctamente, la almacenamos en global
-                        global.titulo = configDatos.nombre;
-                        const base64String = configDatos.logo.toString('base64');
-                        const imageUrl = `data:image/png;base64,${base64String}`;
-                        global.logo = imageUrl;
-                        global.gama = configDatos.gama;
-                        global.direccion = configDatos.direccion;
-                    } // Renderizamos la página principal con la información de todos los destinos
-                    res.render('mensajes', {
-                        mensajes: datos, logo: global.logo, titulo: global.titulo, gama: global.gama,
-                        direccion: global.direccion,sesion :req.session.usuario
-                    });
-                });
-            } else {
-                res.render('mensajes', {
-                    mensajes: datos, logo: global.logo, titulo: global.titulo, gama: global.gama,
-                    direccion: global.direccion, sesion :req.session.usuario
-                });//Cargo la ventana destino con los usuarios no validados
-            }
+            res.json({mensajes : datos, usuario : req.session.usuario})
         }
+    })
+
     });
 
-});
+
 
 router.post('/mandarMensaje', function (req, res, next) {
     const DAOAp = require('../mysql/daoMensajes')
     const midao = new DAOAp(pool)
+    console.log("ASDFASF")
 
     var date = new Date().toISOString()
-    datos={
-        correoEmisor : req.body.correoEmisor, 
-        correoReceptor : req.body.correoReceptor,
-        cuerpoMensaje : req.body.cuerpoMensaje,
+    datos = {
+        correoEmisor: req.body.correoEmisor,
+        correoReceptor: req.body.correoReceptor,
+        cuerpoMensaje: req.body.cuerpoMensaje,
         fecha: date,
-        rolEmisor :req.session.usuario.rol,
+        rolEmisor: req.session.usuario.rol,
     }
 
     midao.altaMensaje(datos, (err, datos) => {
         if (err) {
             res.json("0")
         } else {
+            console.log("1223123")
             res.json(datos)
         }
     })
