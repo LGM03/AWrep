2966d63c737aea6a8e2f936529d91d91bbe4a4f8
LGM03 <99087911+LGM03@users.noreply.github.com>
2023-12-17 20:02:59 +0100

    commit

--- public/js/mensajes.js
+++ public/js/mensajes.js
@@ -3,19 +3,25 @@ $(function () {
   $("#botonMensajesRecibidos").on("click", function () {//ver mensajes recibidos
     $("#mensajesRecibidos").fadeIn(1000)
     $("#mensajesEnviados").hide()
+    
+    $("#mensajesVacio").addClass('d-none')
     $("#mensajesRecibidos .cajaMensaje").hide()
     $.ajax({
       method: "GET",
       url: "/mensajes/leerEnviados",
-      data: {  },
+      data: {},
       success: function (datos, state, jqXHR) { //Si todo ha ido bien pongo un mensaje de acierto
         if (datos !== "0") {
-          $("#CorreoReceptor").text(datos.usuario)
-          datos.mensajes.forEach(element => {
-            if(element.correoReceptor == datos.usuario.correo){
-              agregarCajaRecibidosMensajes(element,$("#cajamensajesRecibidos"))
-            }
-          });
+          if (datos.mensajes.length == 0) {
+            $("#mensajesVacio").removeClass('d-none')
+          } else {
+            $("#CorreoReceptor").text(datos.usuario)
+            datos.mensajes.forEach(element => {
+              if (element.correoReceptor == datos.usuario.correo) {
+                agregarCajaRecibidosMensajes(element, $("#cajamensajesRecibidos"))
+              }
+            });
+          }
         } else {
           alert("Ha ocurrido un error con los mensajes")
         }
@@ -29,22 +35,27 @@ $(function () {
   $("#botonMensajesEnviados").on("click", function () {// ver mensajes que he enviado
     $("#mensajesEnviados").fadeIn(1000)
     $("#mensajesRecibidos").hide()
-    
+    $("#mensajesVacio").addClass('d-none')
+
     $("#mensajesEnviados .cajaMensaje").hide()
 
     $.ajax({
       method: "GET",
       url: "/mensajes/leerEnviados",
-      data: {  },
+      data: {},
       success: function (datos, state, jqXHR) { //Si todo ha ido bien pongo un mensaje de acierto
         if (datos !== "0") {
-          $("#CorreoEmisor").text(datos.usuario)
-          datos.mensajes.forEach(element => {
-            if(element.correoEmisor == datos.usuario.correo){
-    
-              agregarCajaEnviadosMensajes(element,$("#cajamensajesEnviados"))
-            }
-          });
+          if (datos.mensajes.length == 0) {
+            $("#mensajesVacio").removeClass('d-none')
+          } else {
+            $("#CorreoEmisor").text(datos.usuario)
+            datos.mensajes.forEach(element => {
+              if (element.correoEmisor == datos.usuario.correo) {
+
+                agregarCajaEnviadosMensajes(element, $("#cajamensajesEnviados"))
+              }
+            });
+          }
         } else {
           alert("Ha ocurrido un error con los mensajes")
         }
@@ -56,44 +67,45 @@ $(function () {
   })
 
   $("#idMandarMensaje").on("click", function (event) {  //TODO validacion en cliente de valores
-     
+
     event.preventDefault()
     var datosMensaje = {
       correoEmisor: $("#correoEmisor").prop("value"),
       correoReceptor: $("#correoReceptor").prop("value"),
       cuerpoMensaje: $("#cuerpoMensaje").prop("value"),
-      fecha : new Date().toLocaleString()
-      }
+      fecha: new Date().toLocaleString()
+    }
 
-      $.ajax({
-        method: "POST",
-        url: "/mensajes/mandarMensaje",
-        data: datosMensaje,
-        success: function (datos, state, jqXHR) { //Si todo ha ido bien pongo un mensaje de acierto
-          if (datos !== "0") {
-            $("#MandarMensaje").modal('hide')
-            if(!$("#cajamensajesEnviados").hasClass('d-none')){
-              actualizarCajaEnviadosMensajes(datosMensaje,$("#cajamensajesEnviados"))
-            }
-            $("#correoReceptor").prop("value","")
-            $("#cuerpoMensaje").prop("value","")
-
-            alert("Mensaje enviado con éxito ")
-          } else {
-            alert("Correo Destinatario no válido")
+    $.ajax({
+      method: "POST",
+      url: "/mensajes/mandarMensaje",
+      data: datosMensaje,
+      success: function (datos, state, jqXHR) { //Si todo ha ido bien pongo un mensaje de acierto
+        if (datos !== "0") {
+          $("#MandarMensaje").modal('hide')
+          if (!$("#cajamensajesEnviados").hasClass('d-none')) {
+            $("#mensajesVacio").removeClass('d-none')
+            actualizarCajaEnviadosMensajes(datosMensaje, $("#cajamensajesEnviados"))
           }
-        },
-        error: function (jqXHR, statusText, errorThrown) { //Si ha ocurrido un error pongo un mensaje de error
-          alert("Ha ocurrido un error enviando el mensaje")
+          $("#correoReceptor").prop("value", "")
+          $("#cuerpoMensaje").prop("value", "")
+          
+          alert("Mensaje enviado con éxito ")
+        } else {
+          alert("Correo Destinatario no válido")
         }
-      });
-    
+      },
+      error: function (jqXHR, statusText, errorThrown) { //Si ha ocurrido un error pongo un mensaje de error
+        alert("Ha ocurrido un error enviando el mensaje")
+      }
+    });
+
   })
 })
 
-function agregarCajaRecibidosMensajes(mensaje, padre){// saco la cakja de los mensajes recibidos
+function agregarCajaRecibidosMensajes(mensaje, padre) {// saco la cakja de los mensajes recibidos
 
-  
+
   var fecha = new Date(mensaje.fecha).toLocaleString()
   var cajaMensaje = `<div class="row align-items-center m-2 my-2 cajaMensaje">
   <div class="col-12 cajamensaje d-flex flex-column">
@@ -111,13 +123,13 @@ function agregarCajaRecibidosMensajes(mensaje, padre){// saco la cakja de los me
   </div>
 </div>`
 
-padre.append(cajaMensaje)
-padre.removeClass("d-none")
+  padre.append(cajaMensaje)
+  padre.removeClass("d-none")
 
 
 }
 
-function agregarCajaEnviadosMensajes(mensaje, padre){ // saco la caja de los mensajes enviados 
+function agregarCajaEnviadosMensajes(mensaje, padre) { // saco la caja de los mensajes enviados 
 
   var fecha = new Date(mensaje.fecha).toLocaleString()
   var cajaMensaje = `<div class="row align-items-center m-2 my-2 cajaMensaje">
@@ -136,14 +148,14 @@ function agregarCajaEnviadosMensajes(mensaje, padre){ // saco la caja de los men
   </div>
 </div>`
 
-padre.append(cajaMensaje)
-padre.removeClass("d-none")
+  padre.append(cajaMensaje)
+  padre.removeClass("d-none")
 
 
 }
 
 
-function actualizarCajaEnviadosMensajes(mensaje, padre){// actualizo la caja de los mensajes enviados 
+function actualizarCajaEnviadosMensajes(mensaje, padre) {// actualizo la caja de los mensajes enviados 
 
   var cajaMensaje = `<div class="row align-items-center m-2 my-2 cajaMensaje">
   <div class="col-12 cajamensaje d-flex flex-column">
@@ -161,8 +173,8 @@ function actualizarCajaEnviadosMensajes(mensaje, padre){// actualizo la caja de 
   </div>
 </div>`
 
-padre.children().first().after(cajaMensaje);
-padre.removeClass("d-none")
+  padre.children().first().after(cajaMensaje);
+  padre.removeClass("d-none")
 
 
 }
